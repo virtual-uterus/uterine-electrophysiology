2858fd4c1161c8d28098805ff9ce263c3a792489
Mathias Roesler <mroe734@aucklanduni.ac.nz>
2024-08-09 15:44:58 +1200

    Updated boxplots to show data points

--- code/plots/intervalPlotSetup.m
+++ code/plots/intervalPlotSetup.m
@@ -13,32 +13,21 @@ phases = estrusPhases();
 nb_expts = size(analysis_structs.(phase), 2);
 j = find(lower(phases) == phase);  % Index of the phase
 color = getPhaseColor(phase);
-plotted_data = zeros(2, nb_expts);
-data_pts = [];
+plotted_data = zeros(1, nb_expts); 
+x_centre = j + ((nb_expts - 1) * offset) / 2;
 
 for k = 1:nb_expts
     AS = analysis_structs.(phase)(k); % Analysis structure
-%     data_pts = horzcat(data_pts, ...
-%         AS.("event_interval")(1, (AS.("event_interval")(1, :) > 0)));
-    plotted_data(1, k) = mean(...
+    plotted_data(k) = mean(...
         AS.("event_interval")(1, (AS.("event_interval")(1, :) > 0)));
-    plotted_data(2, k) = avgDeviation(...
-        AS.("event_interval")(2, (AS.("event_interval")(2, :) > 0)), ...
-        AS.("event_interval")(3, (AS.("event_interval")(3, :) > 0)));
-
-
-    % Interval plot    
-    errorbar(j+(k-1)*offset, plotted_data(1, k), plotted_data(2, k), ...
-        '.', 'Color', color)
-    hold on;
-    hold on;
-
 end
 
-% swarmchart(j, data_pts, 20, color, 'filled');
-% hold on;
-% boxplot(data_pts,  'Positions', j, 'Widths', 0.05);
-x_centre = j + ((nb_expts - 1) * offset) / 2;
-boxplot(plotted_data(1, :), 'Positions', x_centre, 'Widths', 0.05);
+% Plot data points
+swarmchart(ones(1, size(plotted_data, 2))*x_centre, ...
+    plotted_data(1, :), 10, color, 'filled', 'XJitterWidth',  0.1);
+hold on;
+
+% Plot box plot
+boxplot(plotted_data(1, :), 'Positions', x_centre);
 ylabel("Event interval (s)")
 end
--- code/plots/velocityPlotSetup.m
+++ code/plots/velocityPlotSetup.m
@@ -13,30 +13,23 @@ phases = estrusPhases();
 nb_expts = size(analysis_structs.(phase), 2);
 j = find(lower(phases) == phase);  % Index of the phase
 color = getPhaseColor(phase);
-plotted_data = zeros(2, nb_expts);
-data_pts = [];
+plotted_data = zeros(1, nb_expts);
+x_centre = j + ((nb_expts - 1) * offset) / 2;
 
 for k = 1:nb_expts
     AS = analysis_structs.(phase)(k); % Analysis structure
     analysed_event_idx = AS.("nb_samples") > 0; % Marked events indices
-    %     data_pts = horzcat(data_pts, AS.("prop_vel")(analysed_event_idx));
     plotted_data(1, k) = mean(AS.("prop_vel")(analysed_event_idx));
-    plotted_data(2, k) = std(AS.("prop_vel")(analysed_event_idx));
 
-    %   % Velocity box plot
-    %     boxplot(AS.("prop_vel")(analysed_event_idx), ...
-    %         'Positions', j+(k-1)*offset, ...
-    %         'Widths', 0.05);
-    errorbar(j+(k-1)*offset, plotted_data(1, k), plotted_data(2, k), ...
-        '.', 'Color', color)
-    hold on;
 end
 
-% swarmchart(j, data_pts, 20, color, 'filled');
-% hold on;
-% boxplot(data_pts,  'Positions', j, 'Widths', 0.05);
-x_centre = j + ((nb_expts - 1) * offset) / 2;
-boxplot(plotted_data(1, :), 'Positions', x_centre, 'Widths', 0.05);
+% Plot data points
+swarmchart(ones(1, size(plotted_data, 2))*x_centre, ...
+    plotted_data(1, :), 10, color, 'filled', 'XJitterWidth',  0.1);
+hold on;
+
+% Plot box plot
+boxplot(plotted_data(1, :), 'Positions', x_centre);
 
 ylabel("Propagation velocity (mm/s)")
 end
