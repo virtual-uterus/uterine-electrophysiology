119323fb0c6430ca425110dbeb5ed0c57af7a4a8
Mathias Roesler <mroe734@aucklanduni.ac.nz>
2024-08-14 11:03:12 +1200

    Changed the duration plot setup to also plot mean for each experiment

--- code/plots/durationPlotSetup.m
+++ code/plots/durationPlotSetup.m
@@ -15,7 +15,7 @@ phases = estrusPhases();
 nb_expts = size(analysis_structs.(phase), 2);
 j = find(lower(phases) == phase);  % Index of the phase
 color = getPhaseColor(phase);
-% plotted_data = zeros(1, nb_expts); 
+mean_data = zeros(1, nb_expts); 
 plotted_data = [];
 x_centre = j + ((nb_expts - 1) * offset) / 2;
 
@@ -33,6 +33,7 @@ for k = 1:nb_expts
     AS = analysis_structs.(phase)(k); % Analysis structure
     % plotted_data(k) = mean(...
     %     AS.(field)(1, (AS.(field)(1, :) > 0)));
+    mean_data(k) = mean(AS.(field)(1, (AS.(field)(1, :) > 0)));
     plotted_data = [plotted_data, AS.(field)(1, (AS.(field)(1, :) > 0))];
 end
 
@@ -41,8 +42,13 @@ swarmchart(ones(1, size(plotted_data, 2))*x_centre, ...
     plotted_data(1, :), 10, color, 'filled', 'XJitterWidth',  0.1);
 hold on;
 
+% Plot mean points
+plot(ones(1, nb_expts) .* x_centre, mean_data, '.k', ...
+    'MarkerSize', 13);
+
 % Plot box plot
 boxplot(plotted_data(1, :), 'Positions', x_centre, 'Colors', color);
 ylabel("Event durations (s)")
+ylim([0, 35])
 
 end
