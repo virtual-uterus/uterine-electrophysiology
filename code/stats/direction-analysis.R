30f98c3754505d21a0fd7b25064e7fead60a5a32
Mathias Roesler <mroe734@aucklanduni.ac.nz>
2024-08-25 13:03:43 +1200

    Wrote a script to plot the propagation direction using stacked bar charts

---
+++ code/stats/direction-analysis.R
@@ -0,0 +1,69 @@
+# direction-analysis.R
+# This script loads and plots the propagation direction
+
+if (!require("tidyverse")) install.packages("tidyverse")
+if (!require("ggplot2")) install.packages("ggplot2")
+if (!require("argparse")) install.packages("argparse")
+
+# Load required packages
+library(argparse)
+library(tidyverse)
+library(ggplot2)
+
+# Source other scripts
+source("utils.R")
+source("plots.R")
+
+# Create parser
+parser <- ArgumentParser(
+  description = "Load and plot the propagation direction."
+)
+parser$add_argument("dir",
+  type = "character",
+  help = "path to data directory from base directory"
+)
+parser$add_argument("metric", type = "character", help = "metric to analyse")
+parser$add_argument("--save-dir",
+  type = "character", default = "electrophys/figures/stats",
+  help = "path to save directory"
+)
+
+# Parse arguments
+args <- parser$parse_args()
+
+# Retrieve arguments
+data_dir <- file.path(base_dir(), args$dir)
+metric <- args$metric
+
+# Load the data
+data <- load_metric(metric, data_dir)
+
+# Combine data
+combined_data <- combine_data(data)
+
+# Reshape the data by pivoting all columns except for 'Phase'
+long_data <- combined_data %>%
+  pivot_longer(
+    cols = -Phase, names_to = "Experiment", values_to = "Direction"
+  ) %>%
+  select(Phase, Experiment, Direction)
+
+# Calculate the percentage of each direction type
+processed_data <- long_data %>%
+  filter(!is.na(Direction)) %>% # Remove NA values
+  group_by(Phase, Direction) %>%
+  summarise(Value = n(), .groups = "drop") %>%
+  group_by(Phase) %>%
+  mutate(Value = 100 * Value / sum(Value)) %>%
+  mutate(Direction = factor(Direction,
+    levels = c("1", "-1", "0"),
+    labels = c("Ovaries → Cervix", "Cervix → Ovaries", "Disorganised")
+  ))
+
+# Plot results
+plot_prop_direction(processed_data)
+
+# Save the plot to a file
+save_file <- paste0(metric, ".png")
+save_path <- file.path(file.path(base_dir(), args$save_dir), save_file)
+ggsave(save_path, plot = last_plot(), dpi = 300)
