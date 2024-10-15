73f2c6dd150e16441cf04cc1447e541669be0dcb
Mathias Roesler <mroe734@aucklanduni.ac.nz>
2024-10-16 11:35:53 +1300

    Changed propagation direction analysis to use vectors from UTEMS code

--- code/analysis/propagationAnalysis.m
+++ code/analysis/propagationAnalysis.m
@@ -21,34 +21,21 @@ prop_trends = zeros(nb_rows, nb_cols);
 prop_directions = ones(1, nb_cols); % Default direction ovaries to cervix
 mean_time = mean(arranged_times, 2, 'omitnan');
 
-%% Estimate propagation direction based on the trend of the mean times
-for k = 1:nb_cols
-    prop_trends(:, k) = trenddecomp(arranged_times(:, k));
-end
-
-[~, min_ind] = min(prop_trends);
-[~, max_ind] = max(prop_trends);
-
-prop_directions(abs(max_ind - min_ind) < round(...
-    0.8 * size(arranged_times, 1))) = 0; % Disorganised activity
-
-prop_directions(min_ind > max_ind) = -1; % Cervix to ovaries
-
-[grp_size, vals] = groupcounts(prop_directions');
-[max_val, max_idx] = max(grp_size); 
-
-if max_val == 2 && numel(grp_size) == 2 && sum(vals) == 0
-    prop_direction = 2; % Split direction
-
-else
-    prop_direction = vals(max_idx); 
-end
-
 %% Estimate propagation distance based on the mean times
 [~, min_ind] = min(mean_time);
 [~, max_ind] = max(mean_time);
 prop_dist = abs(electrode_dist * (max_ind - min_ind));
 
-%% Estimate propagation velocity using UTEMS code
-[~, ~, prop_vel] = calcVelocity(arranged_times, electrode_dist);
+%% Estimate propagation velocity and direction using UTEMS code
+[~, Vy, prop_vel] = calcVelocity(arranged_times, electrode_dist);
+
+% Remove values that are not from marked channels
+nan_chns = find(isnan(arranged_times));
+Vy(nan_chns) = nan;
+prop_vel(nan_chns) = nan;
+
+if not(xor(any(mean(Vy, 'omitnan') > 0), all(mean(Vy, 'omitnan') > 0)))
+    prop_direction = sign(mean(Vy(:, 1), 'omitnan'));
+else
+    prop_direction = 0;
 end
