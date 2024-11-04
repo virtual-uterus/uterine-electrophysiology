671fcc030229852da29e1f72db64ed31cdd2122d
Mathias Roesler <mroe734@aucklanduni.ac.nz>
2024-11-04 15:10:31 +1300

    Updated the burst duration algorithm using an Otsu threshold to detect start and end

--- code/analysis/computeDuration.m
+++ code/analysis/computeDuration.m
@@ -42,42 +42,74 @@ for j = chns
         if var(trend) <= 0.03
             continue
         end
-    
-    else
+
+        % Find peaks and ensure the indices are in a valid range
+        [~, peak_loc] = findpeaks(trend);
+
+        if ~isempty(peak_loc)
+            % If peaks have been found estimate duration
+            % Find peak closest to timestamp
+            [~, idx] = min(abs(peak_loc - round(win_split*length(trend))));
+            peak_idx = peak_loc(idx);
+
+            bin_trend = imbinarize(trend);
+
+            % Check if the value at peak_idx in bin_trend is 1
+            if bin_trend(peak_idx) == 1
+                % Find the start of the connected 1s region
+                first_idx = find( ...
+                    bin_trend(1:peak_idx) == 0, 1, 'last') + 1;
+                if isempty(first_idx)
+                    first_idx = 1; % If no 0s, default to beginning
+                end
+
+                % Find the end of the connected 1s region
+                end_idx = find( ...
+                    bin_trend(peak_idx:end) == 0, 1, 'first') + peak_idx - 2;
+                if isempty(end_idx)
+                    end_idx = length(bin_trend); % If no 0s, default to end
+                end
+
+                start_times(j) = first_idx;
+                end_times(j) = end_idx;
+            end
+        end
+
+    else % If slow-waves
         % Find events using the trend of the signal
         trend = trenddecomp(signal(:, j), 'ssa', ...
             round(nb_samples*sample_percent));
-    end
 
-    % Find peaks and ensure the indices are in a valid range
-    [~, peak_loc] = findpeaks(trend);
+        % Find peaks and ensure the indices are in a valid range
+        [~, peak_loc] = findpeaks(trend);
 
-    if ~isempty(peak_loc)
-        % If peaks have been found estimate duration
-        % Find peak closest to timestamp
-        [~, idx] = min(abs(peak_loc - round(win_split*length(trend))));
-        peak_idx = peak_loc(idx); 
-        grad = gradient(trend);
-        [~, ~, crossing_idx] = zerocrossrate(grad);
-        crossing_idx = find(crossing_idx);
-        
-        % Start index is first direction change before peak
-        [~, start_idx] = min(abs(crossing_idx - peak_idx)); 
-        if start_idx == 1
-            start_idx = 2; % Default to 2 to avoid error
+        if ~isempty(peak_loc)
+            % If peaks have been found estimate duration
+            % Find peak closest to timestamp
+            [~, idx] = min(abs(peak_loc - round(win_split*length(trend))));
+            peak_idx = peak_loc(idx);
+            grad = gradient(trend);
+            [~, ~, crossing_idx] = zerocrossrate(grad);
+            crossing_idx = find(crossing_idx);
+
+            % Start index is first direction change before peak
+            [~, start_idx] = min(abs(crossing_idx - peak_idx));
+            if start_idx == 1
+                start_idx = 2; % Default to 2 to avoid error
+            end
+            start_times(j) = crossing_idx(start_idx-1);
+
+            % Second 0 cross after peak in gradient is return to baseline
+            if length(crossing_idx) >= start_idx + 2
+                end_idx = crossing_idx(start_idx + 2);
+            elseif length(crossing_idx) >= start_idx + 1
+                end_idx = crossing_idx(start_idx + 1); % Underestimate duration
+            else
+                end_idx = nb_samples; % Default to window edge
+            end
+
+            end_times(j) = end_idx;
         end
-        start_times(j) = crossing_idx(start_idx-1);
-
-        % Second 0 cross after peak in gradient is return to baseline
-        if length(crossing_idx) >= start_idx + 2
-            end_idx = crossing_idx(start_idx + 2);
-        elseif length(crossing_idx) >= start_idx + 1
-            end_idx = crossing_idx(start_idx + 1); % Underestimate duration
-        else
-            end_idx = nb_samples; % Default to window edge
-        end
-            
-        end_times(j) = end_idx;
     end
 end
 
